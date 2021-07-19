28422fae114ba7ca222eb70184f51a579db491dd
MasOcelot <45953218+MasOcelot@users.noreply.github.com>
2021-07-19 08:12:31 -0700

    Fencer.java (part 1)
    Wrote tests for Fencer related to FencerID getters/setters.

--- app/src/main/java/com/ocelot/android/poolparty/fencer/Fencer.java
+++ app/src/main/java/com/ocelot/android/poolparty/fencer/Fencer.java
@@ -2,13 +2,61 @@ package com.ocelot.android.poolparty.fencer;
 
 public class Fencer {
 
+    private static int numFencers = 0;
+
+    private int index;
     private Score[] score;
     private FencerID fencerID;
     private CompetitiveSpecs compSpecs;
 
-    public Fencer(String firstName, String lastName, Hand hand, int numCompetitors) {
+    public Fencer(FencerID fencerID, int numCompetitors) {
+        this.index = numFencers;
+        numFencers++;
         this.score = new Score[numCompetitors];
-        this.fencerID = new FencerID(firstName, lastName, hand);
+        this.fencerID = fencerID;
+    }
+
+    public Fencer(FencerID fencerID) {
+        this(fencerID, 1);
+    }
+
+    private FencerID getFencerID() {
+        return fencerID;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public void setFirstName(String firstName) {
+        this.fencerID.setFirstName(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        this.fencerID.setLastName(lastName);
+    }
+
+    public void setHand(Hand hand) {
+        this.fencerID.setHand(hand);
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getFirstName() {
+        FencerID fencerID = getFencerID();
+        return fencerID.getFirstName();
+    }
+
+    public String getLastName() {
+        FencerID fencerID = getFencerID();
+        return fencerID.getLastName();
+    }
+
+    public Hand getHand() {
+        FencerID fencerID = getFencerID();
+        return fencerID.getHand();
     }
 
 }
--- app/src/test/java/com/ocelot/android/poolparty/fencer/FencerTest.java
+++ app/src/test/java/com/ocelot/android/poolparty/fencer/FencerTest.java
@@ -1,4 +1,62 @@
 package com.ocelot.android.poolparty.fencer;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class FencerTest {
+    private Fencer fencer;
+
+    @Before
+    public void setUp() {
+        FencerID fencerID = new FencerID();
+        fencer = new Fencer(fencerID);
+    }
+
+    // Will only work without testIndexCounter()
+//    @Test
+//    public void testDefaultIndex() {
+//        int expectedIndex = 0;
+//
+//        assertEquals(expectedIndex, fencer.getIndex());
+//    }
+
+    @Test
+    public void testIndexCounter() {
+        int expectedIndex1 = 0;
+        int expectedIndex2 = 1;
+
+        FencerID fencerID2 = new FencerID("A", "Harioka", Hand.LEFT);
+        Fencer fencer2 = new Fencer(fencerID2);
+        assertEquals(expectedIndex1, fencer.getIndex());
+        assertEquals(expectedIndex2, fencer2.getIndex());
+    }
+
+    @Test
+    public void testDefaultFencerID() {
+        String expectedFirstName = "First";
+        String expectedLastName = "Last";
+        Hand expectedHand = Hand.RIGHT;
+
+        assertEquals(expectedFirstName, fencer.getFirstName());
+        assertEquals(expectedLastName, fencer.getLastName());
+        assertEquals(expectedHand, fencer.getHand());
+    }
+
+    @Test
+    public void testFencerID() {
+        String expectedFirstName = "Jonathan";
+        String expectedLastName = "Candles";
+        Hand expectedHand = Hand.LEFT;
+
+        fencer.setFirstName("Jonathan");
+        fencer.setLastName("Candles");
+        fencer.setHand(Hand.LEFT);
+
+        assertEquals(expectedFirstName, fencer.getFirstName());
+        assertEquals(expectedLastName, fencer.getLastName());
+        assertEquals(expectedHand, fencer.getHand());
+    }
+
 }
