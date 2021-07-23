ded7c40e950f1b4ee8f46905b77bdf2449e04d48
MasOcelot <45953218+MasOcelot@users.noreply.github.com>
2021-07-23 08:08:15 -0700

    Create Stage.java
    Enum for Stage Tracker Finite State Machine

---
+++ app/src/test/java/com/ocelot/android/poolparty/bout/Stage.java
@@ -0,0 +1,141 @@
+package com.ocelot.android.poolparty.bout;
+
+public enum Stage {
+    PREBOUT {
+        @Override
+        public Stage encounterState() {
+            return ENCOUNTER;
+        }
+
+        @Override
+        public Stage pauseState() {
+            return this;
+        }
+
+        @Override
+        public Stage breakState() {
+            return BREAK;
+        }
+
+        @Override
+        public Stage preBoutState() {
+            return PREBOUT;
+        }
+
+        @Override
+        public Stage postBoutState() {
+            return POSTBOUT;
+        }
+    },
+    ENCOUNTER {
+        @Override
+        public Stage encounterState() {
+            return this;
+        }
+
+        @Override
+        public Stage pauseState() {
+            return PAUSE;
+        }
+
+        @Override
+        public Stage breakState() {
+            return BREAK;
+        }
+
+        @Override
+        public Stage preBoutState() {
+            return PREBOUT;
+        }
+
+        @Override
+        public Stage postBoutState() {
+            return POSTBOUT;
+        }
+    },
+
+    PAUSE {
+        @Override
+        public Stage encounterState() {
+            return ENCOUNTER;
+        }
+
+        @Override
+        public Stage pauseState() {
+            return this;
+        }
+
+        @Override
+        public Stage breakState() {
+            return BREAK;
+        }
+
+        @Override
+        public Stage preBoutState() {
+            return PREBOUT;
+        }
+
+        @Override
+        public Stage postBoutState() {
+            return POSTBOUT;
+        }
+    },
+    BREAK {
+        @Override
+        public Stage encounterState() {
+            return ENCOUNTER;
+        }
+
+        @Override
+        public Stage pauseState() {
+            return PAUSE;
+        }
+
+        @Override
+        public Stage breakState() {
+            return this;
+        }
+
+        @Override
+        public Stage preboutState() {
+            return PREBOUT;
+        }
+
+        @Override
+        public Stage postboutState() {
+            return POSTBOUT;
+        }
+    },
+    POSTBOUT{
+        @Override
+        public Stage encounterState() {
+            return ENCOUNTER;
+        }
+
+        @Override
+        public Stage pauseState() {
+            return PAUSE;
+        }
+
+        @Override
+        public Stage breakState() {
+            return BREAK;
+        }
+
+        @Override
+        public Stage preBoutState() {
+            return PREBOUT;
+        }
+
+        @Override
+        public Stage postBoutState() {
+            return this;
+        }
+    };
+
+    public abstract Stage preBoutState();
+    public abstract Stage encounterState();
+    public abstract Stage pauseState();
+    public abstract Stage breakState();
+    public abstract Stage postBoutState();
+}
