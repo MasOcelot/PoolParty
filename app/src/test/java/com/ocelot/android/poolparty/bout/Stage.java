aa92e6253b3fdaf707b1af556658d44824a7507d
MasOcelot <45953218+MasOcelot@users.noreply.github.com>
2021-07-30 07:28:08 -0700

    Update Stage.java
    using single nextStage() branching, instead of five separate functions

--- app/src/test/java/com/ocelot/android/poolparty/bout/Stage.java
+++ app/src/test/java/com/ocelot/android/poolparty/bout/Stage.java
@@ -2,140 +2,19 @@ package com.ocelot.android.poolparty.bout;
 
 public enum Stage {
     PREBOUT {
-        @Override
-        public Stage encounterState() {
-            return ENCOUNTER;
-        }
-
-        @Override
-        public Stage pauseState() {
-            return this;
-        }
-
-        @Override
-        public Stage breakState() {
-            return BREAK;
-        }
-
-        @Override
-        public Stage preBoutState() {
-            return PREBOUT;
-        }
-
-        @Override
-        public Stage postBoutState() {
-            return POSTBOUT;
-        }
     },
+
     ENCOUNTER {
-        @Override
-        public Stage encounterState() {
-            return this;
-        }
-
-        @Override
-        public Stage pauseState() {
-            return PAUSE;
-        }
-
-        @Override
-        public Stage breakState() {
-            return BREAK;
-        }
-
-        @Override
-        public Stage preBoutState() {
-            return PREBOUT;
-        }
-
-        @Override
-        public Stage postBoutState() {
-            return POSTBOUT;
-        }
     },
 
     PAUSE {
-        @Override
-        public Stage encounterState() {
-            return ENCOUNTER;
-        }
-
-        @Override
-        public Stage pauseState() {
-            return this;
-        }
-
-        @Override
-        public Stage breakState() {
-            return BREAK;
-        }
-
-        @Override
-        public Stage preBoutState() {
-            return PREBOUT;
-        }
-
-        @Override
-        public Stage postBoutState() {
-            return POSTBOUT;
-        }
     },
+
     BREAK {
-        @Override
-        public Stage encounterState() {
-            return ENCOUNTER;
-        }
-
-        @Override
-        public Stage pauseState() {
-            return PAUSE;
-        }
-
-        @Override
-        public Stage breakState() {
-            return this;
-        }
-
-        @Override
-        public Stage preBoutState() {
-            return PREBOUT;
-        }
-
-        @Override
-        public Stage postBoutState() {
-            return POSTBOUT;
-        }
     },
+
     POSTBOUT {
-        @Override
-        public Stage encounterState() {
-            return ENCOUNTER;
-        }
-
-        @Override
-        public Stage pauseState() {
-            return PAUSE;
-        }
-
-        @Override
-        public Stage breakState() {
-            return BREAK;
-        }
-
-        @Override
-        public Stage preBoutState() {
-            return PREBOUT;
-        }
-
-        @Override
-        public Stage postBoutState() {
-            return this;
-        }
     };
-
-    public abstract Stage preBoutState();
-    public abstract Stage encounterState();
-    public abstract Stage pauseState();
-    public abstract Stage breakState();
-    public abstract Stage postBoutState();
+    
+    public abstract Stage nextStage(Bout bout);
 }
